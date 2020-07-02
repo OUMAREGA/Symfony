88e146d16de76953ff76d05f35ca4950784af075
OUMAREGA <oumarmarega18@gmail.com>
2020-07-02 16:57:46 +0200

    MULTIPLY

---
+++ sf_pandora_tp1/src/Controller/MultiplyController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MultiplyController extends AbstractController
+{
+    /**
+     * @Route("/multiply/{xmin}/{xmax}/{ymin}/{ymax} ", name="multiply", requirements={"xmin"="\d+"})
+     */
+    public function multiplyAction($xmin, $xmax, $ymin, $ymax,  SessionInterface $session)
+    {
+
+        #stocke les paramètres en session
+        $session->set('xmin', $xmin);
+        $session->set('xmax', $xmax);
+        $session->set('ymin', $ymin);
+        $session->set('ymax', $ymax);
+
+
+        return $this->render('multiply/index.html.twig', [
+            'xmin' => $xmin,
+            'xmax' => $xmax,
+            'ymin' => $ymin,
+            'ymax' => $ymax,
+        ]);
+    }
+
+    /**
+     * @Route("/sessionmultiply", name="sessionmultiply")
+     */
+    public function sessionmultiply(SessionInterface $session)
+    {
+        #verifie si la session a les donnees xmin, xmax, ymi, ymax
+        if($session->has("xmin") && $session->has("xmax") && $session->has("ymin") && $session->has("ymax")) {
+            #retourne la vue avec les données de la session
+            return $this->render('multiply/index.html.twig', [
+                'xmin' => $session->get('xmin'),
+                'xmax' => $session->get('xmax'),
+                'ymin' => $session->get('ymin'),
+                'ymax' => $session->get('ymax')
+            ]);
+        } else {
+            #retourne les valeurs par defaut
+            return $this->render('addition/addition.html.twig', [
+                'xmin' => 0,
+                'xmax' => 10,
+                'ymin' => 0,
+                'ymax' => 10
+            ]);
+        }
+    }
+}
